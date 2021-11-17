eafcdad293a71f06bbb91aa62a8d4f09508ec3e1
Martin Kavalar <mk@katercalling.com>
2021-11-17 13:03:26 +0100

    Build js and simplify image demo

--- notebooks/viewers/image.clj
+++ notebooks/viewers/image.clj
@@ -1,8 +1,7 @@
 ;; # 🏞 Customizing Fetch
 ;; Showing how to use a custom `fetch-fn` with a `content-type` to let Clerk serve arbitrary things, in this case a PNG image.
 (ns ^:nextjournal.clerk/no-cache image
-  (:require [clojure.java.io :as io]
-            [nextjournal.clerk :as clerk]
+  (:require [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as v])
   (:import (java.net.http HttpRequest HttpClient HttpResponse$BodyHandlers)
            (java.net URI)))
@@ -13,14 +12,10 @@
                                                :nextjournal/value bytes})
                       :render-fn (fn [blob] (v/html [:img {:src (v/url-for blob)}]))}])
 
-(let [request (.. (HttpRequest/newBuilder)
-                  (uri (URI. "https://upload.wikimedia.org/wikipedia/commons/5/57/James_Clerk_Maxwell.png"))
-                  build)]
+(.. (HttpClient/newHttpClient)
+    (send (.. (HttpRequest/newBuilder)
+              (uri (URI. "https://upload.wikimedia.org/wikipedia/commons/5/57/James_Clerk_Maxwell.png"))
+              build) (HttpResponse$BodyHandlers/ofByteArray))
+    body)
 
-  (.. (HttpClient/newHttpClient)
-      (send request (HttpResponse$BodyHandlers/ofByteArray))
-      body))
-
-#_(nextjournal.clerk/serve! {:watch-paths ["notebooks"]})
 #_(nextjournal.clerk/show! "notebooks/viewers/image.clj")
-
